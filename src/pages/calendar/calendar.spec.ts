12daedc862996ee8551da1a405665b385390217f
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2018-08-06 17:09:01 -0400

    tests wip

--- src/pages/calendar/calendar.spec.ts
+++ src/pages/calendar/calendar.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { Component, DebugElement} from '@angular/core';
 import { CalendarPage } from './calendar';
 import {IonicModule, Platform, NavController, NavParams} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -8,7 +8,56 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import {StorageProvider} from "../../providers/storage/storage";
 import { spyOnClass } from 'jasmine-es6-spies';
 
+// @Component({
+//   template: `
+//     <ion-page-calendar
+//       [year]="year"
+//       [month]="month">
+//     </ion-page-calendar>
+//   `
+// })
+// class HostComponent {
+//   // July, 2018.
+//   year = 2018;
+//   month = 6;
+// }
+
 describe('Page1', () => {
+  // let fixture: ComponentFixture<HostComponent>;
+  // let host;
+  // let el;
+  // let storageServiceSpy: jasmine.SpyObj<StorageProvider>;
+  //
+  //
+  //
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     imports: [
+  //       IonicModule.forRoot(CalendarPage)
+  //     ],
+  //     declarations: [ CalendarPage, HostComponent ],
+  //     providers: [
+  //       { provide: StorageProvider, useValue: spyOnClass(StorageProvider) },
+  //       NavController,
+  //       { provide: NavParams, useClass: class {} }
+  //     ],
+  //   })
+  //   .compileComponents();
+  // }));
+  //
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(HostComponent);
+  //   host = fixture.componentInstance;
+  //   storageServiceSpy = TestBed.get(StorageProvider);
+  // });
+  //
+  // beforeEach(() => {
+  //
+  //   storageServiceSpy.getCheckedSlots.and.returnValue([]);
+  //   fixture.detectChanges();
+  //
+  // });
+
   let de: DebugElement;
   let comp: CalendarPage;
   let fixture: ComponentFixture<CalendarPage>;
@@ -23,22 +72,24 @@ describe('Page1', () => {
       providers: [
         NavController,
         StorageProvider,
-        // { provide: NavParams, useClass: spyOnClass(NavParams) }
-        { provide: NavParams, useClass: class {} }
+        { provide: NavParams, useFactory: () => spyOnClass(NavParams) }
       ]
     });
   }));
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CalendarPage);
     navParams = TestBed.get(NavParams);
     comp = fixture.componentInstance;
+    // console.log(fixture.detectChanges());
     // de = fixture.debugElement.query(By.css('h3'));
   });
 
   it('should have expected <h3> text', () => {
-    // fixture.detectChanges();
+    fixture.detectChanges();
     expect(true).toBe(true);
+
     // const h3 = de.nativeElement;
     // expect(h3.innerText).toMatch(/ionic/i,
     //   '<h3> should say something about "Ionic"');
